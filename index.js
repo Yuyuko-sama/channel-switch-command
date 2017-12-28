fb723adeb8f50717511fe78a8196fd587a158943
Snugglez <32309975+Snugglez@users.noreply.github.com>
2017-12-28 10:29:05 -0500

    sum formating

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const Command = require('Command')
+const Command = require('command')
 module.exports = function csc(dispatch) {
-const c = Command(dispatch)
-let czone = null;
-dispatch.hook('S_LOAD_TOPO', 1, (event) => {   		
-czone = event.zone
-})
-c.add('ch', (chan) => {
-c.message(`Moving to channel ${chan}`)
-dispatch.toServer('C_SELECT_CHANNEL', 1, {
-unk: 1,
-zone: czone,
-channel: parseInt(chan) - 1
-})})}
+	const c = Command(dispatch)
+	
+	let czone = null;
+	
+	dispatch.hook('S_LOAD_TOPO', 1, event => {
+		czone = event.zone
+	})
+	
+	c.add('ch', chan => {
+		c.message(`Moving to channel ${chan}`)
+		dispatch.toServer('C_SELECT_CHANNEL', 1, {
+			unk: 1,
+			zone: czone,
+			channel: parseInt(chan) - 1
+		})
+	})
+}
