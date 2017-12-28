8520c8b7a99842859597e3a3c26d4d414e2c23e2
Snugglez <32309975+Snugglez@users.noreply.github.com>
2017-12-28 10:30:50 -0500

    nope, like this :^)

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const Command = require('command')
+const Command = require('Command')
 module.exports = function csc(dispatch) {
-	const c = Command(dispatch)
-	
-	let czone = null;
-	
-	dispatch.hook('S_LOAD_TOPO', 1, event => {
-		czone = event.zone
-	})
-	
-	c.add('ch', chan => {
-		c.message(`Moving to channel ${chan}`)
-		dispatch.toServer('C_SELECT_CHANNEL', 1, {
-			unk: 1,
-			zone: czone,
-			channel: parseInt(chan) - 1
-		})
-	})
-}
+const c = Command(dispatch)
+let czone = null;
+dispatch.hook('S_LOAD_TOPO', 1, (event) => {   		
+czone = event.zone
+})
+c.add('ch', (chan) => {
+c.message(`Moving to channel ${chan}`)
+dispatch.toServer('C_SELECT_CHANNEL', 1, {
+unk: 1,
+zone: czone,
+channel: parseInt(chan) - 1
+})})}
