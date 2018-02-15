f2af8a15b91b640a19eac523d912d953cf9f4679
Snugglez <32309975+Snugglez@users.noreply.github.com>
2018-02-14 23:45:41 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const Command = require('Command')
 module.exports = function csc(dispatch) {
 const c = Command(dispatch)
 let czone = null;
+let chanx = null;
 dispatch.hook('S_LOAD_TOPO', 1, (event) => {   		
 czone = event.zone
+})
+dispatch.hook('S_CURRENT_CHANNEL', 2, (event) => {   		
+chanx = event.channel
 })
 c.add('ch', (chan) => {
 c.message(`Moving to channel ${chan}`)
@@ -11,4 +15,13 @@ dispatch.toServer('C_SELECT_CHANNEL', 1, {
 unk: 1,
 zone: czone,
 channel: parseInt(chan) - 1
-})})}
+})})
+c.add('chx', () => {
+c.message(`Moving to next channel`)
+dispatch.toServer('C_SELECT_CHANNEL', 1, {
+unk: 1,
+zone: czone,
+channel: chanx
+})})
+
+}
