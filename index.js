080d678cfa5da828f00d68815f14e3060cf931e5
Snugglez <32309975+Snugglez@users.noreply.github.com>
2018-08-21 10:35:56 -0400

    damn it caali

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const Command = require('Command')
-module.exports = function csc(dispatch) {
-const c = Command(dispatch)
+module.exports = function csc(d) {
 let czone = null;
 let chanx = null;
-dispatch.hook('S_LOAD_TOPO', 3, (event) => {   		
+d.hook('S_LOAD_TOPO', 3, (event) => {   		
 czone = event.zone
 })
-dispatch.hook('S_CURRENT_CHANNEL', 2, (event) => {   		
+d.hook('S_CURRENT_CHANNEL', 2, (event) => {   		
 chanx = event.channel
 })
-c.add('ch', (chan) => {
-c.message(`Moving to channel ${chan}`)
-dispatch.toServer('C_SELECT_CHANNEL', 1, {
-unk: 1,
-zone: czone,
-channel: parseInt(chan) - 1
-})})
-c.add('chx', () => {
-c.message(`Moving to next channel`)
-dispatch.toServer('C_SELECT_CHANNEL', 1, {
+d.command.add("ch", {
+x() {
+d.command.message(`Moving to next channel`)
+d.send('C_SELECT_CHANNEL', 1, {
 unk: 1,
 zone: czone,
 channel: chanx
-})})}
+})
+},
+$default(chan) {
+d.command.message(`Moving to channel ${chan}`)
+d.send('C_SELECT_CHANNEL', 1, {
+unk: 1,
+zone: czone,
+channel: parseInt(chan) - 1
+})
+}
+})
+}
