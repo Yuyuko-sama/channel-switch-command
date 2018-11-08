14d1390c6504def6c0e15a117c4a3d4796123797
Yuyuko <40657629+Yuyuko-sama@users.noreply.github.com>
2018-11-09 01:35:02 +0800

    mod.XXX instead

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const Command = require('Command')
-module.exports = function csc(dispatch) {
-const c = Command(dispatch)
+//
+module.exports = function csc(mod) {
 let czone = null;
 let chanx = null;
-dispatch.hook('S_LOAD_TOPO', 3, (event) => {   		
+mod.hook('S_LOAD_TOPO', 3, (event) => {   		
 czone = event.zone
 })
-dispatch.hook('S_CURRENT_CHANNEL', 2, (event) => {   		
+mod.hook('S_CURRENT_CHANNEL', 2, (event) => {   		
 chanx = event.channel
 })
-c.add('ch', (chan) => {
-c.message(`Moving to channel ${chan}`)
-dispatch.toServer('C_SELECT_CHANNEL', 1, {
+mod.command.add('ch', (chan) => {
+mod.command.message(`Moving to channel ${chan}`)
+mod.send('C_SELECT_CHANNEL', 1, {
 unk: 1,
 zone: czone,
 channel: parseInt(chan) - 1
 })})
-c.add('chx', () => {
-c.message(`Moving to next channel`)
-dispatch.toServer('C_SELECT_CHANNEL', 1, {
+mod.command.add('chx', () => {
+mod.command.message(`Moving to next channel`)
+mod.send('C_SELECT_CHANNEL', 1, {
 unk: 1,
 zone: czone,
 channel: chanx
